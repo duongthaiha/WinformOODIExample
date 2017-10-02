d3fc17cdc2ea9d3f24c239452af4c49bf0371705
Ha Thai Duong <duongthaiha@gmail.com>
2017-10-02 11:04:24 +0100

    Initial commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Main : Form
+    {
+        private readonly DatabaseOperations _databaseOperations;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public Main(DatabaseOperations databaseOperations) : this()
+        {
+            _databaseOperations = databaseOperations;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _databaseOperations.AddOperation();
+        }
+    }
+}
